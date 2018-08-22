8cf900e2b13c5f2c675f7a34a8049e12dba64f5d
handboy <handersonmedeiros@gmail.com>
2018-08-22 00:16:06 -0300

    Starting with jobs imgs

---
+++ boot/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/webcane/pen/lHGJf.
+
+ Example of responsive image thumbnail grid. 
+Each image give id to populate the Bootstrap Modal with filtered images for the hidden images repository. 
+Navigate gallery images inside modal using the Carousel.
---
+++ boot/js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+   /* activate the carousel */
+   $("#modal-carousel").carousel({interval:false});
+
+   /* change modal title when slide changes */
+   $("#modal-carousel").on("slid.bs.carousel",       function () {
+        $(".modal-title")
+        .html($(this)
+        .find(".active img")
+        .attr("title"));
+   });
+
+   /* when clicking a thumbnail */
+   $(".row .img-thumbnail").click(function(){
+    var content = $(".carousel-inner");
+    var title = $(".modal-title");
+  
+    content.empty();  
+    title.empty();
+  
+  	var id = this.id;  
+     var repo = $("#img-repo .carousel-item");
+     var repoCopy = repo.filter("#" + id).clone();
+     var active = repoCopy.first();
+  
+    active.addClass("active");
+    title.html(active.find("img").attr("title"));
+  	content.append(repoCopy);
+    
+    // show the modal
+  	$("#modal-gallery").modal("show");
+  });
+
+});
